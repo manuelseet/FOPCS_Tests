30ce805edde12313ac605b39ac1a8b97335349c0
Manuel Seet <e0838385@u.nus.edu>
2021-10-29 07:15:06 +0800

    Day 5 Quiz

--- QuizDay5.cs
+++ QuizDay5.cs
@@ -10,7 +10,40 @@ namespace FOPCS_Tests
     {
         static void Main(string[] arg)
         {
+            int[,] theArray = new int[,]
+            {
+                {5,3},{2,9},{2,9},
+                {5,1},{7,1},{8,3},
+                {0,3},{2,2},{9,7}
+            };
 
+            int count = 0;
+
+            Console.Write("Please enter a number (0-9): ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            DigitCounter(theArray, input);
+
+        }
+
+        public static void DigitCounter(int[,] arr, int i)
+        {
+            int rows = arr.GetLength(0); //get number of rows in array
+            int col = arr.GetLength(1); //get number of col in array
+
+            int count = 0; //prepare a counter variable
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < col; c++)
+                {
+                    if (arr[r, c] == i)
+                        count++;
+                }
+
+
+            }
+
+            Console.WriteLine($"The number of {i} is {count}");
         }
     }
 }
